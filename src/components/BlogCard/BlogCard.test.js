c9589a72cef67db2113dbd918ee3356f8e5c682b
GeoSauer <geoffrey.sauer89@gmail.com>
2023-01-17 15:30:07 -0700

    add author to blogCard

--- src/components/BlogCard/BlogCard.test.js
+++ src/components/BlogCard/BlogCard.test.js
@@ -1,11 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import BlogCard from './BlogCard';
 
-test('should render the blog card with title, subtitle, author and text', () => {
+test.only('should render the blog card with title, subtitle, author and text', () => {
   const container = render(
     <BlogCard
       title="Test title"
       subtitle="Test subtitle"
+      authors="Test Author"
       text="lorem ipsum blah blah blah"
       image="https://www.fillmurray.com/300/200"
     />
